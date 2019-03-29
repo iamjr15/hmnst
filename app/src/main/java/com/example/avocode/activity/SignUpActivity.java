b28ef023e4f3bf1ee647ca9822edaba32570c827
Nikunj kakadiya <deskcodesolution3008@gmail.com>
2019-03-29 09:03:47 +0530

    Changes DOB and Gender UI

--- app/src/main/java/com/example/avocode/activity/SignUpActivity.java
+++ app/src/main/java/com/example/avocode/activity/SignUpActivity.java
@@ -1,10 +1,16 @@
 package com.example.avocode.activity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.example.avocode.AvaApplication;
 import com.example.avocode.R;
@@ -14,6 +20,10 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -37,12 +47,21 @@ public class SignUpActivity extends AppCompatActivity {
     EditText lastname_et;
 
     @BindView(R.id.dob_et)
-    EditText dob_et;
+    TextView dob_et;
+
+    @BindView(R.id.rbMale)
+    RadioButton rbMale;
+
+    @BindView(R.id.rbFemale)
+    RadioButton rbFemale;
 
     @BindView(R.id.gender_et)
-    EditText gender_et;
+    RadioGroup genderEt;
+
 
     private Util util;
+    final Calendar myCalendar = Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +69,35 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
         ButterKnife.bind(this);
         util = new Util(this);
+
+
+        date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateText();
+            }
+
+        };
+
+        dob_et.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(SignUpActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    void updateText() {
+        String myFormat = "dd/MM/yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        dob_et.setText(sdf.format(myCalendar.getTime()));
     }
 
     @OnClick(R.id.next_signup_button)
@@ -93,12 +141,18 @@ public class SignUpActivity extends AppCompatActivity {
         }
     }
 
+    String gender;
+
     private void addNewRegisteredUser() {
         util.hideLoading();
+        gender = rbMale.isChecked() ?
+                getString(R.string.male) :
+                getString(R.string.female);
+
         Intent intent = new Intent(SignUpActivity.this, SignUpTwoActivity.class);
         intent.putExtra("firstName", firstname_et.getText().toString().trim());
         intent.putExtra("lastName", lastname_et.getText().toString().trim());
-        intent.putExtra("gender", gender_et.getText().toString().trim());
+        intent.putExtra("gender", gender);
         intent.putExtra("dob", dob_et.getText().toString().trim());
         intent.putExtra("password", passwordSignUp_et.getText().toString().trim());
         intent.putExtra("phone", phonenumber_et.getText().toString().trim());
